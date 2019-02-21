f3e803baead2fa566a5a03b8a4a4049a9b6f3fe6
DavideGiovanelli <d.giov.lli@gmail.com>
2019-02-21 16:21:24 +0100

    Added two more View Controllers

---
+++ Pireps_Tabbed/ThirdViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ThirdViewController.swift
+//  Pireps_Tabbed
+//
+//  Created by student on 21.02.19.
+//  Copyright © 2019 ZHAW. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+}
